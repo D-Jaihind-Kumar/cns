e0ca207b97d495c12ce759b04551ffe9497b28bf
D Jaihind Kumar <66009855+djaihind@users.noreply.github.com>
2020-12-10 09:44:37 +0530

    Update md5.java

--- md5.java
+++ md5.java
@@ -1,13 +1,13 @@
 import java.math.BigInteger;
 import java.security.MessageDigest;
-class SHA
+class MD
 {
   public static void main(String[] args) throws java.lang.Exception
   {
     try
     {
       String s= "Hello World";
-      MessageDigest md = MessageDigest.getInstance("SHA-1");
+      MessageDigest md = MessageDigest.getInstance("MD5);
       byte[] digestInfo = md.digest(s.getBytes());
       BigInteger n = new BigInteger(1,digestInfo);
       String hashtxt = n.toString(16);
