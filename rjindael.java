7175068bc3646e79dc8c6f161e9c11c282d51dd1
D Jaihind Kumar <66009855+djaihind@users.noreply.github.com>
2020-12-10 10:03:20 +0530

    Create rjindael.java

---
+++ rjindael.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import javax.crypto.*;
+class Des{
+        public static void main(String args[])
+        {
+                try{
+                        KeyGenerator kg=KeyGenerator.getInstance("Rjindael");
+                        SecretKey skey = kg.generateKey();
+                        Cipher c=Cipher.getInstance("DES");
+                        c.init(Cipher.ENCRYPT_MODE,skey);
+                        byte[] txt = "hi".getBytes();
+                        byte[] ntxt = c.doFinal(txt);
+                        System.out.println(new String(ntxt));
+                        c.init(Cipher.DECRYPT_MODE, skey);
+                }
+                catch(Exception e)
+                {
+                  System.out.println("Error "+e+" occured");
+                }
+        }
+}
